d9170809da20fc8b44222f9afd2765155f61e52c
leeheeweon <ggoddii@naver.com>
2021-10-31 16:13:06 +0900

    첫 커밋

---
+++ src/main/java/com/kaoni/theCulureOfLearning/TheCulureOfLearningApplication.java
@@ -0,0 +1,13 @@
+package com.kaoni.theCulureOfLearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheCulureOfLearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheCulureOfLearningApplication.class, args);
+	}
+
+}
